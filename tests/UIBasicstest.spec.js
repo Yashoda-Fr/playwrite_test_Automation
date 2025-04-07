a0e27e4b10c8a1b11bbe3f3f3f356f8651daa653
yashoda-fr <fyashodha@gmail.com>
2025-04-07 09:29:45 +0530

    init

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,11 @@
+const { test } = require('@playwright/test');
+
+test('Browser Context Test', async ({ browser }) => {
+  const context = await browser.newContext(); // Fresh browser instance
+  const page = await context.newPage();       // New tab
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/'); // Go to URL
+});
+
+test('Page Fixture Test', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/'); // Browser + page auto-created
+  });
