cc866849d8a8dc5bea9760ea2224271819ebb773
yashoda-fr <fyashodha@gmail.com>
2025-04-26 05:04:53 +0530

    refactor: enhance browser context test with login functionality and assertions

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,11 +1,18 @@
-const { test } = require('@playwright/test');
+const { test,expect } = require('@playwright/test');
 
-test('Browser Context Test', async ({ browser }) => {
+test.only('Browser Context Test', async ({ browser }) => {
   const context = await browser.newContext(); // Fresh browser instance
   const page = await context.newPage();       // New tab
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/'); // Go to URL
+  await page.locator("#username").fill('rahulshetty'); // Type username
+  await page.locator("#password").fill('learning'); // Type password
+  await page.locator("#signInBtn").click(); // Click sign in button
+  console.log(await page.locator("[style*='block']").textContent()); // Print error message
+  await expect (page.locator("[style*='block']")).toContainText('Incorrect'); // Assert error message
 });
 
 test('Page Fixture Test', async ({ page }) => {
-    await page.goto('https://rahulshettyacademy.com/loginpagePractise/'); // Browser + page auto-created
+    await page.goto('https://www.google.com/'); // Browser + page auto-created
+    console.log(await page.title()); // Print the title of the page
+    await expect(page).toHaveTitle('Google'); // Assert the URL
   });
